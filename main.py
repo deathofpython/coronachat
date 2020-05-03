cebf97f4bae7ec2be8e898eb5a0c5b9022d2cc46
home_computer <dalekx350@gmail.com>
2020-05-03 21:55:35 +1000

    project

--- main.py
+++ main.py
@@ -151,3 +151,12 @@ def start_chat(username1, username2):
         elif request.form['button'] == 'Search':
             session["id"] = request.form["id"]
             return redirect('/answer')
+
+
+def main():
+    db_session.global_init('db/blogs.sqlite')
+    app.run(port=80, host='0.0.0.0')
+
+
+if __name__ == '__main__':
+    main()
